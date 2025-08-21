391ed89a778fc2da90cd33ffbfca31703b222b45
Renat Moroz <bs4123555@gmail.com>
2025-08-22 01:13:29 +0300

    fixed main

--- src/main.js
+++ src/main.js
@@ -34,15 +34,16 @@ form.addEventListener('submit', event => {
       }
 
       createGallery(data.hits);
-      setTimeout(hideLoader, 100);
       form.reset();
     })
     .catch(error => {      
       iziToast.error({
         title: 'Error',
         message: 'Something went wrong. Please try again later.',
-      })
+      });
       console.error(error);
-      hideLoader();
     })
+    .finally(() => {
+      hideLoader();
     });
+});
